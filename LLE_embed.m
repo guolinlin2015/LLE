3dcd57e309a8e78f0a2a7db1b8cf75e4b79a9e9a
sjstiver <shannonjstiverson@gmail.com>
2017-05-22 22:40:53 -0600

    Update LLE_embed.m
    Implemented the method from other code for calculating (I-W)'(I-W)
    Still not working...

--- LLE_embed.m
+++ LLE_embed.m
@@ -1,14 +1,27 @@
-function Y = LLE_embed(W,d)
+function Y = LLE_embed(W,nbd,d)
 % Takes matrix W with columns as weights and solves eigenvector problem to
 % find imbedding into d dimensions 
 
 % d = desired embedding dimension
 % W = matrix of LLE weights (including zeros)
 % Y = matrix of embedding coordinates (n x d)
+nbd
+[k,n] = size(W);
 
-n = size(W,1);
+M = sparse(1:n,1:n,ones(1,n),n,n,4*k*n);
+% M = eye(n);
+
+% Compute (I-W)'(I-W)
+for i=1:n
+   w = W(:,i) % Weights are columns
+   j = nbd(i,:) % Neighbor indices are rows
+   M(i,j) = M(i,j) - w';
+   M(j,i) = M(j,i) - w;
+   M(j,j) = M(j,j) + w*w';
+end
+
 opts.isreal = 1;
 opts.issym = 1;
-[Y,evals] = eigs((eye(n)-W)'*(eye(n)-W),d+1,'SM',opts);
+[Y,evals] = eigs(M,d+1,'SM',opts);
 evals
 Y = Y(:,2:d+1).*sqrt(n);
