1c73a8ac1be7dc46ed68233087db1478ee9085e6
sjstiver <shannonjstiverson@gmail.com>
2017-05-26 10:35:56 -0600

    update
    final version of embedding file

--- LLE_embed.m
+++ LLE_embed.m
@@ -5,23 +5,24 @@ function Y = LLE_embed(W,nbd,d)
 % d = desired embedding dimension
 % W = matrix of LLE weights (including zeros)
 % Y = matrix of embedding coordinates (n x d)
-nbd
+
 [k,n] = size(W);
 
 M = sparse(1:n,1:n,ones(1,n),n,n,4*k*n);
-% M = eye(n);
 
 % Compute (I-W)'(I-W)
 for i=1:n
-   w = W(:,i) % Weights are columns
-   j = nbd(i,:) % Neighbor indices are rows
+   w = W(:,i); % Weights are columns
+   j = nbd(:,i); % Neighbor indices are rows
    M(i,j) = M(i,j) - w';
    M(j,i) = M(j,i) - w;
    M(j,j) = M(j,j) + w*w';
 end
+M;
 
 opts.isreal = 1;
 opts.issym = 1;
-[Y,evals] = eigs(M,d+1,'SM',opts);
+opts.disp = 0;
+[Y,evals] = eigs(M,d+1,0,opts);
 evals
-Y = Y(:,2:d+1).*sqrt(n);
+Y = Y(:,2:(d+1)).*sqrt(n);
