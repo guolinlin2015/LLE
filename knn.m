7084fbf0f91263d6f8528fc14f72edc1c4012823
sjstiver <shannonjstiverson@gmail.com>
2017-05-26 10:37:45 -0600

    update
    switched rows, columns to match weight file

--- knn.m
+++ knn.m
@@ -2,26 +2,23 @@ function index = knn(k,X)
 % Finds k nearest neighbors of all points in X
 
 % k = desired number of neighbors
-% X = n x d matrix of data points - dimension d, n points
-% index = n x k matrix of indices for neighbors
-[n,~] = size(X);
+% X = d x n matrix of data points - dimension d, n points
+% index = k x n matrix of indices for neighbors
+[~,n] = size(X);
 
 % Calculate pairwise distances for points in X
-X2 = sum(X.^2,2);
-dist = repmat(X2,1,n) + repmat(X2',n,1) - 2*X*X';
+X2 = sum(X.^2,1);
+dist = repmat(X2,n,1) + repmat(X2',1,n) - 2*(X'*X);
 
 % Sort
-[~,index] = sort(dist,2,'ascend');
+[~,index] = sort(dist,1,'ascend');
 
 % Check for case where k > n, return index of k points for each
 if k >= n
-index = index(:,2:end);
+index = index(2:end,:);
 else
-    index = index(:,2:(k+1));
+    index = index(2:(k+1),:);
 end
-
-% Calculate Euclidean distances
-
 
 return
 
