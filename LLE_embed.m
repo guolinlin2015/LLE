8979942fc7af4666c02d157463d75be33305aa4c
sjstiver <shannonjstiverson@gmail.com>
2017-10-31 21:47:06 -0600

    Updated for current version of matlab as of 10/31

--- LLE_embed.m
+++ LLE_embed.m
@@ -9,6 +9,7 @@ function Y = LLE_embed(W,nbd,d)
 [k,n] = size(W);
 
 M = sparse(1:n,1:n,ones(1,n),n,n,4*k*n);
+% M = zeros(n);
 
 % Compute (I-W)'(I-W)
 for i=1:n
@@ -18,11 +19,13 @@ for i=1:n
    M(j,i) = M(j,i) - w;
    M(j,j) = M(j,j) + w*w';
 end
-M;
 
 opts.isreal = 1;
 opts.issym = 1;
 opts.disp = 0;
-[Y,evals] = eigs(M,d+1,0,opts);
-evals
-Y = Y(:,2:(d+1)).*sqrt(n);
+[Y,evals] = eigs(M,12,'SM',opts);
+
+format long
+evals = diag(evals);
+z = find(evals <= eps);
+Y = Y(:,(min(z)-1):-1:(min(z)-d)).*sqrt(n);
